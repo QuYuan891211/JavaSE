5fba2aa115e1fd0f8fdf00a1f84e19548d8b246b
quyuan <quyuan891211@163.com>
2018-08-10 23:54:19 +0800

    JosephLoop

--- src/Array/Test.java
+++ src/Array/Test.java
@@ -6,7 +6,8 @@ import java.util.*;
 public class Test {
     public static void main(String[] a){
         int[] target = {4,5,6,4,4,5,5,5,3,5,5,7,5,3,5};
-        System.out.println(find(target));
+        //System.out.println(find(target));
+        josephProblem();
     }
 //1.找到数组中的支配者[出现率大于0.5],如果没有，则返回-1（方法：额外开辟空间存储已查过的数）
     public static int find(int[] target) {
@@ -35,7 +36,36 @@ public class Test {
         }
         return -1;
     }
+    //2.约瑟环问题（数组的方式---被提出的人也参与报数）
+    public static void josephProblem(){
+        Boolean[] cycle = new Boolean[30];
+        for(int i=0;i<cycle.length;i++){
+            cycle[i] = true;
+        }
+        System.out.println("此时数组：" + Arrays.toString(cycle) );
+        int leftPerson = cycle.length;
+        //下一次循环的起始索引
+        int index = 0;
+        //记录步数
+        int count = 0;
+        while (leftPerson > 15){
+            count++;
+            if(count==9){
+                if(cycle[index]){
+                    cycle[index]=false;
+                    leftPerson --;
+                    count = 0;
+                    System.out.println("排除出第" + index + "人");
+                    System.out.println("此时数组：" + Arrays.toString(cycle) );
+                }else {
+                    count--;
+                }
+            }
+            index++;
+            if(index==cycle.length){index=0;}
+        }
     }
+}
 
     //打印方法
 
